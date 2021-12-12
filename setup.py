bf19c45660c1e632335d59f0ffd75b979134f09f
MisterMine01 <pokemovie06@gmail.com>
2021-12-12 16:49:25 +0100

    add pypi

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="pcjs_api",
+    version="1.0",
+    description="Php cursor",
+    url="https://github.com/MisterMine01/PCJsApi",
+    author="MisterMine01",
+    py_modules=['pcjs_api'],
+    package_dir={"": "Client/python"},
+    install_requires=[
+        "requests"
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3"
+    ],
+    keywords=["server", "cursor"]
+)
