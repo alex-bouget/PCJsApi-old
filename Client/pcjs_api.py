eced48bbee535909a1b4264fadc52a81ca84ca93
abouget <alexandre.bouget@etu.u-bordeaux.fr>
2022-03-29 18:02:33 +0200

    add doc

--- Client/pcjs_api.py
+++ Client/pcjs_api.py
@@ -1,17 +1,25 @@
 import json
-from requests import get, post
+from turtle import st
+from typing import Dict
+from urllib import request
+from requests import get, post, Response
 
 
 class PCJsApi:
-    def __init__(self, url):
+    url: str
+    System: dict
+
+    def __init__(self, url: str):
         self.url = url
         self.System = json.loads(get(self.url, {"MM1_jc": "200"}).text)
 
-    def getResolveBySystem(self, name, post_data=None):
+    def getResolveBySystem(self, name: str, post_data: dict = None) -> request.Response:
+        """Get the response of the server with this cursor"""
         if post_data is None:
             post_data = {}
 
-        def getUrl(url, get_data):
+        def getUrl(url: str, get_data: dict):
+            """Get the url with GET data"""
             data_get = []
             for key, value in get_data.items():
                 data_get.append(key + "=" + value)
@@ -19,6 +27,7 @@ class PCJsApi:
         resolve = post(getUrl(self.url, self.System[name]["GET"]), data=post_data)
         return resolve
 
-    def getJsBySystem(self, name, post_data=None):
+    def getJsBySystem(self, name: str, post_data: dict = None):
+        """Get json response of the server with this cursor"""
         resolve = self.getResolveBySystem(name, post_data)
         return json.loads(resolve.text)
